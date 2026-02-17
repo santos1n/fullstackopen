e8f21d19bad6225da93119188990e2b85327d367
Nelson Marc Santos <marcnelsonsantos21+github@gmail.com>
2026-02-17 17:29:29 +0800

    changes and build for render

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 var morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
@@ -10,6 +11,8 @@ app.use(morgan("tiny"));
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body"),
 );
+app.use(cors());
+app.use(express.static("dist"));
 
 let persons = [
   {
@@ -105,12 +108,7 @@ const generateId = () => {
   return String(maxId + 1);
 };
 
-function assignId(req, res, next) {
-  req.id = uuid.v4();
-  next();
-}
-
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
