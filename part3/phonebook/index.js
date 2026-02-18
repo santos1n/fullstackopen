b98b5d0fa4f542f8a7cb3969a8931d0ca44e3abf
Nelson Marc Santos <marcnelsonsantos21+github@gmail.com>
2026-02-18 10:58:11 +0800

    changes for render

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 var morgan = require("morgan");
-const cors = require("cors");
+// const cors = require("cors");
 
 const app = express();
 
@@ -11,7 +11,7 @@ app.use(morgan("tiny"));
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body"),
 );
-app.use(cors());
+// app.use(cors());
 app.use(express.static("dist"));
 
 let persons = [
